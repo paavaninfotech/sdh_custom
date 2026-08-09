38af3facaa9688910571e81c7397faf4e7d2fae6
Divyesh Mangroliya <mangroliya@Divyeshs-MacBook-Air.local>
2026-08-09 11:14:57 +0530

    Added Bulk Return Functionality

--- sdh_custom/overrides/delivery_note.py
+++ sdh_custom/overrides/delivery_note.py
@@ -9,3 +9,12 @@ class CustomDeliveryNote(DeliveryNote):
             pass
         else:
             super().validate_return_against()
+    
+    def check_sales_return_reference(self):
+        # Override core method to allow standalone returns without return_against
+        if self.is_return:
+            # Skip core check that mandates self.return_against
+            pass
+        else:
+            super().validate_return_against()
+    
